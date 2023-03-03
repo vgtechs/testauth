1b06fbe1f94ea8a68545ab372cb8adb263211783
vgtechs <vgtechs@gmail.com>
2023-03-03 13:15:00 +0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -4,14 +4,14 @@ $email = $_POST['email'];
 $psw = $_POST['psw'];
 
 $users_array = [
-               ["user1@gmail.com", "psw1", "Иван"],
-               ["user2@gmail.com", "psw2", "Петр"],
-               ["user3@gmail.com", "psw3", "Николай"],
-               ["user4@gmail.com", "psw4", "Арестарх"],
-               ];
+    ["user1@gmail.com", "psw1", "Иван"],
+    ["user2@gmail.com", "psw2", "Петр"],
+    ["user3@gmail.com", "psw3", "Николай"],
+    ["user4@gmail.com", "psw4", "Арестарх"],
+];
 
-foreach ($users_array as $ua){
-    if($ua[0] == $email && $ua[1] == $psw){
+foreach ($users_array as $ua) {
+    if ($ua[0] == $email && $ua[1] == $psw) {
         session_start();
         $_SESSION['email'] = $ua[0];
         $_SESSION['name'] = $ua[2];
@@ -22,6 +22,6 @@ foreach ($users_array as $ua){
 }
 
 http_response_code(403);
-    exit();
+exit();
 
 
